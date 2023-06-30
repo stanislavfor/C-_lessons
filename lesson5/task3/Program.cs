11bc62fa78b147f50f09ee7a1ebfec53f4b28d7a
stanislavfor <stasposter@narod.ru>
2023-06-30 05:27:10 +0300

    lesson5 hw

--- lesson5/task3/Program.cs
+++ lesson5/task3/Program.cs
@@ -2,7 +2,6 @@
 // 4; массив[6, 7, 19, 345, 3]->нет
 // - 3; массив[6, 7, 19, 345, 3]->да
 
-
 int[] FillArrayWithRandomNumber(int size)
 {
   int[] arr = new int[size];
@@ -14,43 +13,43 @@ int[] FillArrayWithRandomNumber(int size)
   return arr;
 }
 
-//bool CheckNumber(int[] arr, int number)
-//{
-//  for (int i = 0; i < arr.Length; i++)
-//  {
-//    if (number == arr[i])
-//    {
-//      return true;
-//   }
-// }
-//  return false;
-//}
+bool CheckNumber(int[] arr, int number)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (number == arr[i])
+    {
+      return true;
+    }
+  }
+  return false;
+}
 
-System.Console.Write("Введите количсество элементов массива: ");
+System.Console.Write("Введите кол-во элементов массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
 int[] array = FillArrayWithRandomNumber(length);
 System.Console.WriteLine(string.Join("; ", array));
 System.Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-// if (CheckNumber(array, number))
-// {
-// System.Console.WriteLine("Элемент найден");
-// }
-// else
-// {
-// System.Console.WriteLine("Элемент НЕ найден");
-// }
+if (CheckNumber(array, number))
+{ 
+  System.Console.WriteLine($"Элемент '{number}' есть в массиве");
+}
+else
+{
+  System.Console.WriteLine($"Элемент '{number}' НЕ найден");
+}
 
 // Вариант 2
 
-string isOk = "Элемент НЕ найден";
-for (int i = 0; i < array.Length; i++)
-{
-  if (number == array[i])
-  {
-    isOk = "Элемент найден";
-    break;
-  }
-}
-System.Console.WriteLine(isOk);
+//string isOk = "Элемент НЕ найден";
+//for (int i = 0; i < array.Length; i++)
+//{
+//  if (number == array[i])
+//  {
+//    isOk = "Элемент найден";
+//    break;
+//  }
+//}
+//System.Console.WriteLine(isOk);
