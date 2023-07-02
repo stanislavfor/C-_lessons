d06c1d287e19dfec71d8b384d9f738eb7c9f2ef0
stanislavfor <stasposter@narod.ru>
2023-07-02 20:46:21 +0300

    add hw2 of lesson7

--- lesson7/hw2/Program.cs
+++ lesson7/hw2/Program.cs
@@ -4,3 +4,35 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа НЕТ в массиве
+
+
+int m = 3; // Количество строк
+int n = 4; // Количество столбцов
+Random random = new Random();
+int[,] arr = new int[m, n];
+Console.WriteLine("Задан исходный массив:");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+  for (int j = 0; j < arr.GetLength(1); j++)
+  {
+    arr[i, j] = random.Next(10, 99);
+    Console.Write("{0} ", arr[i, j]);
+  }
+  Console.WriteLine();
+}
+
+Console.Write("Введите строку: ");
+int mNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int nNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+
+if (mNumber < 0 | mNumber > arr.GetLength(0) - 1 | nNumber < 0 | nNumber > arr.GetLength(1) - 1)
+{
+  Console.WriteLine("Такого числа НЕТ в массиве");
+}
+else
+{
+  Console.WriteLine("Найден элемент массива -> {0}", arr[mNumber, nNumber]);
+}
+//Console.ReadLine();
+Console.WriteLine();
