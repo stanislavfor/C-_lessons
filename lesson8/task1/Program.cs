afc016b8f630184565d2530383b4c7ff78b9f507
stanislavfor <stasposter@narod.ru>
2023-07-05 00:31:07 +0300

    add lesson8 fix

---
+++ lesson8/task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая меняет местами первую и последнюю строку массива.
+
+
+
+void FillMatrixWithRandom(int[,] matrix)
+{
+  Random rnd = new Random();
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rnd.Next(0, 10);
+    }
+  }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      System.Console.Write($"{matrix[i, j]} \t");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+System.Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+
+int temp = 0;
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+  temp = matrix[0, j];
+  matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+  matrix[matrix.GetLength(0) - 1, j] = temp;
+}
+
+System.Console.WriteLine();
+PrintMatrix(matrix);
---
+++ lesson8/task2/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+
+void FillMatrixWithRandom(int[,] matrix)
+{
+  Random rnd = new Random();
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rnd.Next(0, 10);
+    }
+  }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      System.Console.Write($"{matrix[i, j]} \t");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+System.Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+
+int[,] resultMatrix = new int[column, row];
+
+for (int i = 0; i < resultMatrix.GetLength(0); i++)
+{
+  for (int j = 0; j < resultMatrix.GetLength(1); j++)
+  {
+    resultMatrix[i, j] = matrix[j, i];
+  }
+}
+System.Console.WriteLine();
+PrintMatrix(resultMatrix);
+
+
